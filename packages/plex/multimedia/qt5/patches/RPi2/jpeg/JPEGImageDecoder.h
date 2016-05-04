145d73ec4214402138920c96c851a80626fb5da4
longchair <longchair@hotmail.com>
2016-05-04 13:04:28 +0200

    qt5 : fix & refactor RPi HW Jpeg decoding

--- packages/plex/multimedia/qt5/patches/RPi2/jpeg/JPEGImageDecoder.h
+++ packages/plex/multimedia/qt5/patches/RPi2/jpeg/JPEGImageDecoder.h
@@ -24,53 +24,70 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef RPIImageDecoder_h
-#define RPIImageDecoder_h
+#ifndef JPEGImageDecoder_h
+#define JPEGImageDecoder_h
 
-#include "ImageDecoder.h"
-#include "brcmimage.h"
+#include "platform/image-decoders/RPIImageDecoder.h"
+#include "platform/image-decoders/brcmimage.h"
 #include "interface/mmal/mmal.h"
 
 namespace blink
 {
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+    // JPEG header struct
+#define JFIF_DATA_SIZE 4096 // additionnal data size for header parsing
+
+    typedef unsigned char BYTE;
+
+    typedef struct _JFIFHeader
+    {
+        BYTE SOI[2];          /* 00h  Start of Image Marker     */
+        BYTE APP0[2];         /* 02h  Application Use Marker    */
+        BYTE Length[2];       /* 04h  Length of APP0 Field      */
+        BYTE Identifier[5];   /* 06h  "JFIF" (zero terminated) Id String */
+        BYTE Version[2];      /* 07h  JFIF Format Revision      */
+        BYTE Units;           /* 09h  Units used for Resolution */
+        BYTE Xdensity[2];     /* 0Ah  Horizontal Resolution     */
+        BYTE Ydensity[2];     /* 0Ch  Vertical Resolution       */
+        BYTE XThumbnail;      /* 0Eh  Horizontal Pixel Count    */
+        BYTE YThumbnail;      /* 0Fh  Vertical Pixel Count      */
+        BYTE data[JFIF_DATA_SIZE];
+    } JFIFHEAD;
+
+
     ///////////////////////////////////////////////////////////////////////////////////////////////////
     // This class decodes the JPEG image format.
-    class PLATFORM_EXPORT RPIImageDecoder : public ImageDecoder
+    class PLATFORM_EXPORT JPEGImageDecoder : public RPIImageDecoder
     {
-        WTF_MAKE_NONCOPYABLE(RPIImageDecoder);
+        WTF_MAKE_NONCOPYABLE(JPEGImageDecoder);
     public:
-        RPIImageDecoder(ImageSource::AlphaOption, ImageSource::GammaAndColorProfileOption, size_t maxDecodedBytes);
-        virtual ~RPIImageDecoder();
+        JPEGImageDecoder(AlphaOption, GammaAndColorProfileOption, size_t maxDecodedBytes);
+        ~JPEGImageDecoder() override;
 
         // ImageDecoder
-        virtual String filenameExtension() const override { return "jpg"; }
-        virtual char* platformDecode() { return (char*)"JPEG"; }
+        String filenameExtension() const override { return "jpg"; }
         IntSize decodedSize() const override { return m_decodedSize; }
         virtual bool setSize(unsigned width, unsigned height) override;
+        virtual char* platformDecode() { return (char*)"JPEG"; }
+        virtual bool readSize(unsigned int &width, unsigned int &height);
 
-        virtual bool readSize(unsigned int &width, unsigned int &height) { return false; }
+
         unsigned desiredScaleNumerator() const;
-        virtual unsigned int getMMALImageType() { return MMAL_ENCODING_JPEG; }
 
-        virtual BRCMIMAGE_T* getDecoder() { return NULL; }
-        virtual BRCMIMAGE_REQUEST_T *getDecoderRequest() { return NULL; }
-
-        void log(const char * format, ...);
-    protected:
-        bool m_hasAlpha;
-
-    private:
-        void setDecodedSize(unsigned width, unsigned height);
-
-        void decodeSize() override { decode(true); }
-        void decode(size_t) override { decode(false); }
 
         // Decodes the image.  If |onlySize| is true, stops decoding after
         // calculating the image size.  If decoding fails but there is no more
         // data coming, sets the "decode failure" flag.
         void decode(bool onlySize);
 
+    private:
+        void decodeSize() override { decode(true); }
+        void decode(size_t) override { decode(false); }
+
         IntSize m_decodedSize;
+        void setDecodedSize(unsigned width, unsigned height);
+
     };
 
 } // namespace blink
