c2788ca24fe06cb13a374ef29e4cee2b56d42cdd
longchair <longchair@hotmail.com>
2016-08-24 10:47:49 +0200

    openssh : re-Add ssh by default for all platforms
    This is something we used to have previously, rather than enabling it per platform.

--- packages/network/openssh/package.mk
+++ packages/network/openssh/package.mk
@@ -73,5 +73,10 @@ post_makeinstall_target() {
 }
 
 post_install() {
+  ### PLEX
+  mkdir -p $INSTALL/usr/share
+  cp $PKG_DIR/sshd.conf $INSTALL/usr/share/
+  ### END PLEX 
+
   enable_service sshd.service
 }
