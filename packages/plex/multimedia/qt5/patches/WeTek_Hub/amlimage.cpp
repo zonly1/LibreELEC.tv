60dc88552638b8e8e79d191fa39184b2a13f94f3
LongChair <longchair@hotmail.com>
2016-09-26 08:52:53 +0200

    AML : Jpeg HW decoding preparation work
    this introduces classes & modules for jpeg HW decoding in qtwebengine.

---
+++ packages/plex/multimedia/qt5/patches/WeTek_Hub/amlimage.cpp
@@ -0,0 +1,7 @@
+/** \file
+ *  Jpeg encoder and decoder library using the hardware jpeg codec
+ */
+
+#include "amlimage.h"
+#include "stdio.h"
+
---
+++ packages/plex/multimedia/qt5/patches/WeTek_Hub/amlimage.h
@@ -0,0 +1,5 @@
+
+#ifndef AML_IMAGE_H
+#define AML_IMAGE_H
+
+#endif /* AML_IMAGE_H */
