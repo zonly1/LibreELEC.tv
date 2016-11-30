a9bc3806a1998a38f0ad65da2805a2463e258940
LongChair <LongChair@hotmail.com>
2016-11-30 07:36:31 +0100

    plexmediaplayer : fix webclient deployement

--- packages/plex/mediacenter/plexmediaplayer/package.mk
+++ packages/plex/mediacenter/plexmediaplayer/package.mk
@@ -146,7 +146,7 @@ makeinstall_target() {
   mkdir -p $INSTALL/usr/share/${MEDIACENTER,,} $INSTALL/usr/share/${MEDIACENTER,,}/scripts
   cp -R $ROOT/$PKG_BUILD/resources/* ${INSTALL}/usr/share/${MEDIACENTER,,}
   cp $PKG_DIR/scripts/plex_update.sh ${INSTALL}/usr/share/${MEDIACENTER,,}/scripts/
-  cp -R $ROOT/$PKG_BUILD/.$TARGET_NAME/web-client* ${INSTALL}/usr/share/${MEDIACENTER,,}/
+  cp -R $ROOT/$PKG_BUILD/.$TARGET_NAME/src/web-client* ${INSTALL}/usr/share/${MEDIACENTER,,}/
 
  debug_strip $INSTALL/usr/bin
 }
